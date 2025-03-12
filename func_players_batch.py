2592b3bb9cbf675367ea7f16431b3a42c82002af
Cédric Dada <cedric@ns2.hwdomain.io>
2025-03-12 13:21:41 +0100

    ready

--- func_players_batch.py
+++ func_players_batch.py
@@ -13,13 +13,21 @@ from matplotlib import pyplot as plt
 
 import cv2
 import sys
+import os
 import time
-
+from dotenv import load_dotenv
+# Charger les variables du fichier .env
+load_dotenv()
 
 import torch
 
 #from yolov6.utils.events import LOGGER, load_yaml
-sys.path.append(r"/home/fenaux/Documents/YOLOv6")
+yolov6_path = os.getenv("YOLOV6_location")
+if yolov6_path:
+    sys.path.append(yolov6_path)
+else:
+    print("Erreur : La variable YOLOv6_location n'est pas définie dans le fichier .env")
+
 import yolov6 as bid # trick to avoid Key Eroor on second runtime
 from yolov6.layers.common import DetectBackend
 from yolov6.utils.nms import non_max_suppression
@@ -27,7 +35,12 @@ from yolov6.core.inferer import Inferer
 
 from typing import List, Optional
 
-sys.path.append(r"/home/fenaux/Documents/athle")
+athle_path = os.getenv("athle_location")
+if athle_path:  # Vérifier que la variable est bien définie
+    sys.path.append(athle_path)
+else:
+    print("Erreur : La variable athle_location n'est pas définie dans le fichier .env")
+
 from yolo.yolov6_utils import check_img_size, process_image_array
 
 
@@ -46,7 +59,7 @@ def func_box(video_name: str, save_box_name: str, start_frame: int, end_frame: i
     cuda = device != 'cpu' and torch.cuda.is_available()
     device = torch.device('cuda:0' if cuda else 'cpu')
     
-    local_checkpoint = f"/home/fenaux/Documents/YOLOv6/{checkpoint}.pt"
+    local_checkpoint = yolov6_path+"/{checkpoint}.pt"
     
     model = DetectBackend(local_checkpoint, device=device)
     stride = model.stride
